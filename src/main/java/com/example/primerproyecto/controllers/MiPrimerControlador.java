d0037c7c194b2d7d341b8e70464f336022ffa5d3
oscmar <oscar.tena.marin@gmail.com>
2023-12-14 19:43:06 +0100

    Initial commit

---
+++ src/main/java/com/example/primerproyecto/controllers/MiPrimerControlador.java
@@ -0,0 +1,16 @@
+package com.example.primerproyecto.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class MiPrimerControlador {
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("hora", LocalDateTime.now());
+        return "index";
+    }
+}
